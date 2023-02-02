a2319a15a84a7a901584e76c7cc197541f88cb11
Bogdan Bosca <bogdan-dan.bosca@endava.com>
2023-02-02 20:55:50 +0200

    test(shared): add test for getCSSVariableValue

--- packages/bee-q/src/shared/utils/__tests__/cssVariables.spec.ts
+++ packages/bee-q/src/shared/utils/__tests__/cssVariables.spec.ts
@@ -1,10 +1,40 @@
-import { getColorCSSVariable } from '..';
+import { getColorCSSVariable, getCSSVariableValue } from '..';
 
-describe('cssVariables - getColorCSSVariable()', () => {
-  it('should return the correct CSS Custom Property string', () => {
-    const token = 'ui--primary';
-    const result = `var(--bq-${token})`;
+describe('cssVariables', () => {
+  describe('cssVariables - getColorCSSVariable', () => {
+    it('should return the correct CSS Custom Property string', () => {
+      const token = 'ui--primary';
+      const result = `var(--bq-${token})`;
 
-    expect(getColorCSSVariable(token)).toEqual(result);
+      expect(getColorCSSVariable(token)).toEqual(result);
+    });
+  });
+
+  describe('cssVariables - getCSSVariableValue', () => {
+    beforeEach(() => {
+      jest.spyOn(global, 'getComputedStyle').mockImplementationOnce(
+        () =>
+          ({
+            getPropertyValue: (property) => {
+              if (property === '--size') {
+                return '20px';
+              }
+              return '';
+            },
+          } as unknown as CSSStyleDeclaration),
+      );
+    });
+
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
+
+    it('should return the correct CSS Custom Property string', () => {
+      const element = document.createElement('div');
+      const variable = 'size';
+      const result = '20px';
+
+      expect(getCSSVariableValue(variable, element)).toEqual(result);
+    });
   });
 });
